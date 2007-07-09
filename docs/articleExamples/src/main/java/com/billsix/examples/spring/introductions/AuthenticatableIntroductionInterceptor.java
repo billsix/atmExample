cceb6ebfca6582c4014740e76917831d48768a33
billsix <billsix@b863487e-c834-0410-95f9-84c7addb97e6>
2007-07-09 04:32:35 +0000

    added an Introduction example
    git-svn-id: https://svn.code.sf.net/p/atmexample/code/trunk@3 b863487e-c834-0410-95f9-84c7addb97e6

---
+++ docs/articleExamples/src/main/java/com/billsix/examples/spring/introductions/AuthenticatableIntroductionInterceptor.java
@@ -0,0 +1,52 @@
+/*
+ Copyright (c) 2007 Bill Six
+ 
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+ 
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+ 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ */
+package com.billsix.examples.spring.introductions;
+
+import com.billsix.examples.spring.Authenticatable;
+import java.lang.reflect.Method;
+import org.springframework.aop.support.DelegatingIntroductionInterceptor;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+public class AuthenticatableIntroductionInterceptor extends DelegatingIntroductionInterceptor {
+    
+    public void authenticate(String username, String password) {
+        _isAuthenticated = true;
+    }
+    public boolean isValidUser() {
+        return _isAuthenticated;
+    }
+    
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        Method method = invocation.getMethod();
+        Object[] arguments = invocation.getArguments();
+        if(method.equals(Authenticatable.class.getMethod("authenticate", String.class, String.class)) ) {
+            authenticate(((String)arguments[0]),((String) arguments[1]));
+            return null;
+        } else if( isValidUser()){
+            return invocation.proceed();
+        }
+        throw new IllegalStateException("User is not authenticated");
+    }
+    
+    private boolean _isAuthenticated = false;
+}
---
+++ docs/articleExamples/src/test/java/com/billsix/examples/spring/introductions/CalculatorServiceTest.java
@@ -0,0 +1,65 @@
+/*
+ Copyright (c) 2007 Bill Six
+ 
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+ 
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+ 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ */
+package com.billsix.examples.spring.introductions;
+import com.billsix.examples.spring.Authenticatable;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.springframework.aop.framework.ProxyFactoryBean;
+
+/**
+ *
+ * @author Bill Six
+ */
+public class CalculatorServiceTest extends TestCase {
+    public CalculatorServiceTest( String testName ) {
+        super( testName );
+    }
+    
+    public static Test suite() {
+        return new TestSuite( CalculatorServiceTest.class );
+    }
+    
+    public void setUp() throws Exception{
+        CalculatorServiceImplementation target = new CalculatorServiceImplementation();
+        ProxyFactoryBean factory = new ProxyFactoryBean();
+        factory.addAdvisor(new AuthenticatableIntroductionAdvisor());
+        factory.setTarget(target);
+        factory.setProxyInterfaces(new Class[]{CalculatorService.class, Authenticatable.class});
+        _calculatorService = (CalculatorService) factory.getObject();
+    }
+    
+    public void testInvalidAccess() {
+        try{
+            _calculatorService.add(4,5);
+            fail("The user did not log in yet");
+        } catch(IllegalStateException ise) {
+        }
+    }
+    
+    public void testValidAccess() {
+        ((Authenticatable)_calculatorService).authenticate("","");
+        _calculatorService.add(4,5);
+    }
+    
+    CalculatorService _calculatorService ;
+}
